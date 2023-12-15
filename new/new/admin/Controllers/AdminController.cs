552f7b620525d32d42611029335619893d45571d
dinesh0544 <138740979+dinesh0544@users.noreply.github.com>
2023-12-15 06:36:32 +0530

    Add files via upload

---
+++ new/new/admin/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using admin.context;
+using admin.model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace admin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly appdbcontext _authcontext;
+        public AdminController(appdbcontext appdbcontext)
+        {
+            _authcontext = appdbcontext;
+        }
+        [HttpPost("adminlogin")]
+        public async Task<IActionResult> adminlogin([FromBody] admins userobj)
+        {
+            if (userobj == null)
+                return BadRequest();
+
+            var user = await _authcontext.adminss.FirstOrDefaultAsync(x => x.username == userobj.username && x.password == userobj.password);
+            if (user == null)
+                return NotFound(new { message = "user not found!" });
+            return Ok(new { message = "login sucess!" });
+        }
+    }
+}
